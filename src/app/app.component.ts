c36a8b4c3696fd70ae887336e5dc5b776705a64a
flaviorocks <faio16@gmail.com>
2021-04-05 09:05:09 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,6 @@ import { Component, VERSION } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  name = "Default Angular viwer";
-
   public get pdfUrl(): string {
     const url =
       "https://ng2-pdfjs-viewer.azurewebsites.net/assets/pdfjs/web/pdf-sample.pdf";
